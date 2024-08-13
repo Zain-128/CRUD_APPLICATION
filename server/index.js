d81fc7d7090c2f431ba0e09f229aba58f2d5d80e
Zain-128 <100517579+Zain-128@users.noreply.github.com>
2024-08-13 11:25:04 -0700

    Connecting DB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import { ConnectToMongo } from "./config/connect.db.js";
 
 dotenv.config();
+
+ConnectToMongo();
 const app = express();
 
 app.listen(process.env.PORT, console.log("Server is Listening !"));
