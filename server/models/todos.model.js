088b385caa8dd7b0f92b02302431987156fcdd1e
Zain-128 <100517579+Zain-128@users.noreply.github.com>
2024-08-13 11:31:00 -0700

    Create Models

---
+++ server/models/todos.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title is Required !"],
+  },
+  description: {
+    type: String,
+    required: [true, "Description is Required !"],
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+});
+
+module.exports = mongoose.model("todo", todoSchema);
---
+++ server/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Email is Required !"],
+    unique: [true, "Email must be Unique !"],
+  },
+  name: {
+    type: String,
+    required: [true, "Email is Required !"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is Required !"],
+  },
+  todos: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "todo",
+    },
+  ],
+});
+
+module.exports = mongoose.model("user", userSchema);
